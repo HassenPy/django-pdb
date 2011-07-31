e204953664876431c11e1d16448ac1dd0b91cd73
Tom Christie <tom@tomchristie.com>
2011-07-31 15:53:46 +0200

    Edited setup.py via GitHub

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_long_description():
 setup(
     name='django-pdb',
     version=get_version('django_pdb'),
-    description="Add '--pdb' option to 'manage.py runserver' and 'manage.py test'",
+    description="Gives you 'manage.py runserver --pdb' and 'manage.py test --pdb'",
     long_description=get_long_description(),
     author='Tom Christie',
     author_email='tom@tomchristie.com',
